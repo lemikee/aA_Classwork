8e085c3ed71abcfdbd2b15d35d8237c9eda9541f
Mike <letumichael@gmail.com>
2021-07-23 16:22:39 -0700

    Added ability to remove and add child node with node validation

--- W3D5/Poly Tree Node/lib/00_tree_node.rb
+++ W3D5/Poly Tree Node/lib/00_tree_node.rb
@@ -15,4 +15,12 @@ class PolyTreeNode
         @parent = node
         node.children << self unless node.nil?
     end
+
+    def add_child(node)
+        node.parent = self
+    end
+
+    def remove_child(node)
+        self.children.include?(node) ? (node.parent = nil) : (raise "Child does not exist")
+    end
 end
