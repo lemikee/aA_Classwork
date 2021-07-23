b80afde071689711bbf9cc4f183a79df998ea80a
Mike <letumichael@gmail.com>
2021-07-23 15:27:47 -0700

    Finished #build_move_treeto create the tree we will use bfs on to find the shortest path

--- W3D5/Knight Travails/knights_travails.rb
+++ W3D5/Knight Travails/knights_travails.rb
@@ -13,31 +13,29 @@ class KnightPathFinder
         [-2, -2]
     ]
 
-    attr_reader :start, :move_tree
+    attr_reader :root_node, :move_tree
 
     def initialize(start)
-        @start = start
+        @root_node = PolyTreeNode.new(start)
         @move_tree = []
         build_move_tree
     end
 
     def build_move_tree
-        root_node = PolyTreeNode.new(start)
+        POSSIBLE_MOVES.each do |move|
+            new_x = root_node[0] + move[0]
+            new_y = root_node[1] + move[1]
 
-        queue = [root_node]
-        until queue.empty?
-            current_node = queue.shift
+            next unless new_x.between?(0, 7) && new_y.between?(0, 7) # checks to see if coordinate are in valid range of the 8 x 8 grid
 
+            unless root_node.parent.nil?
+                next if parent.value = [new_x, new_y] # to check if new potential node is NOT the same as the parent
+
+            end
+            
+            new_node = PolyTreeNode.new([new_x, new_y])
+            move_tree << new_node
         end
-
     end
-
-
-
-
-    
-
-
-
 
 end
