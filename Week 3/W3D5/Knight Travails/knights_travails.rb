127b9bd411325fbfb111a7a2c4ddde502f294f54
Mike <letumichael@gmail.com>
2021-07-26 10:44:04 -0700

    Refactored ::valid_moves

--- Week 3/W3D5/Knight Travails/knights_travails.rb
+++ Week 3/W3D5/Knight Travails/knights_travails.rb
@@ -13,32 +13,16 @@ class KnightPathFinder
         [-2, -2]
     ]
 
-    attr_reader :root_node, :move_tree
+    attr_reader :root_node, :considered_positions
 
     def initialize(start)
         @root_node = PolyTreeNode.new(start)
-        @move_tree = []
+        @considered_positions = [start]
         build_move_tree
     end
 
     def self.valid_moves(move)
-        POSSIBLE_MOVES.each do |move|
-        new_x = root_node[0] + move[0]
-        new_y = root_node[1] + move[1]
-
-        next unless new_x.between?(0, 7) && new_y.between?(0, 7) # checks to see if coordinate are in valid range of the 8 x 8 grid
-
-         unless root_node.parent.nil?
-                next if parent.value = [new_x, new_y] # to check if new potential node is NOT the same as the parent
-
-        end
-
-        new_node = PolyTreeNode.new([new_x, new_y])
-        move_tree << new_node
-        end
-    end
-
-    def build_move_tree
+        valid_moves_arr = []
         POSSIBLE_MOVES.each do |move|
             new_x = root_node[0] + move[0]
             new_y = root_node[1] + move[1]
@@ -47,12 +31,22 @@ class KnightPathFinder
 
             unless root_node.parent.nil?
                 next if parent.value = [new_x, new_y] # to check if new potential node is NOT the same as the parent
-
             end
 
             new_node = PolyTreeNode.new([new_x, new_y])
-            move_tree << new_node
+            valid_moves_arr << new_node
         end
+
+        valid_moves_arr
+    end
+
+    def build_move_tree
+
+    end
+
+    def new_move_positions(pos)
+         #takes in a current pos, and then gives new possible moves
+         KnightPathFinder.valid_moves(pos).reject { |move| considered_positions.include?(move) }
     end
 
 end
