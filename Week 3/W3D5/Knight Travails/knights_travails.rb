78e4cabf1af74acd6bc5c8878b97d901568d28d9
Mike <letumichael@gmail.com>
2021-07-26 11:55:36 -0700

    Finished KnightPathFinder

--- Week 3/W3D5/Knight Travails/knights_travails.rb
+++ Week 3/W3D5/Knight Travails/knights_travails.rb
@@ -13,25 +13,22 @@ class KnightPathFinder
         [-2, -2]
     ]
 
-    attr_reader :root_node, :considered_positions
+    attr_reader :considered_positions, :start_pos
+    attr_accessor :root_node
 
     def initialize(start)
-        @root_node = PolyTreeNode.new(start)
+        @start_pos = start
         @considered_positions = [start]
         build_move_tree
     end
 
-    def self.valid_moves(move)
+    def self.valid_moves(pos)
         valid_moves_arr = []
         POSSIBLE_MOVES.each do |move|
-            new_x = root_node[0] + move[0]
-            new_y = root_node[1] + move[1]
+            new_x = pos[0] + move[0]
+            new_y = pos[1] + move[1]
 
             next unless new_x.between?(0, 7) && new_y.between?(0, 7) # checks to see if coordinate are in valid range of the 8 x 8 grid
-
-            unless root_node.parent.nil?
-                next if parent.value = [new_x, new_y] # to check if new potential node is NOT the same as the parent
-            end
 
             valid_moves_arr << [new_x, new_y]
         end
@@ -40,6 +37,7 @@ class KnightPathFinder
     end
 
     def build_move_tree
+        self.root_node = PolyTreeNode.new(start_pos)
         our_queue = [root_node]
 
         until our_queue.empty?
@@ -61,4 +59,25 @@ class KnightPathFinder
          .each { |move| considered_positions << move }
     end
 
+    def find_path(ending_pos)
+        ending_node = root_node.dfs(ending_pos)
+
+        trace_path_back(ending_node).map(&:value)
+    end
+
+    def trace_path_back(ending_node)
+        nodes = []
+
+        current_node = ending_node
+        until current_node.nil?
+            nodes.unshift(current_node)
+            current_node = current_node.parent
+        end
+
+        nodes
+    end
 end
+
+kpf = KnightPathFinder.new([0,0])
+p kpf.find_path([7, 6])
+p kpf.find_path([6, 2])
--- Week 3/W3D5/Knight Travails/tree_node.rb
+++ Week 3/W3D5/Knight Travails/tree_node.rb
@@ -11,11 +11,11 @@ class PolyTreeNode
         unless parent.nil?
             parent.children.delete(self)
         end
-        
+
         @parent = node
         node.children << self unless node.nil?
     end
-    
+
     def add_child(node)
         node.parent = self
     end
@@ -43,11 +43,8 @@ class PolyTreeNode
 
         nil
     end
-
-
-
 end
 
-# tree_nodes = ('a'..'g').map { |value| PolyTreeNode.new(value) } 
+# tree_nodes = ('a'..'g').map { |value| PolyTreeNode.new(value) }
 # our_node = tree_nodes[0]
 # our_node.bfs("e")
