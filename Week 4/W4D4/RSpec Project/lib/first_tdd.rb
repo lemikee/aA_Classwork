024d86e65b56e66e31f8917cbe2e3bc208c5507e
Kent Zhang <kentzhang31@yahoo.com>
2021-07-29 11:59:19 -0700

    Working on tranpose specs

--- Week 4/W4D4/RSpec Project/lib/first_tdd.rb
+++ Week 4/W4D4/RSpec Project/lib/first_tdd.rb
@@ -2,12 +2,17 @@ class Array
   def uniq
     result = []
 
-    self.each_with_index do |el, i|
-      temp = self[0...i] + self[i + 1..-1]
-      result << el if temp.include?(el) && !result.include?(el)
-
-    end
-
+    self.each { |el| result << el unless result.include?(el) }
     result
+  end
+
+  def two_sum
+    indices = []
+    self.each_with_index do |n1, idx1|
+      self.each_with_index do |n2, idx2|
+        indices << [idx1, idx2] if idx1 < idx2 && (n1 + n2 == 0)
+      end
+    end
+    indices
   end
 end
--- Week 4/W4D4/RSpec Project/spec/first_tdd_spec.rb
+++ Week 4/W4D4/RSpec Project/spec/first_tdd_spec.rb
@@ -1,9 +1,38 @@
 require 'first_tdd'
 
-describe "#uniq" do
-  it "should remove duplicates from an array" do
-    expect([1, 2, 1, 3, 3].uniq).to eq([1, 2, 3])
+describe Array do
+  describe "#uniq" do
+    it "should remove duplicates from an array" do
+      expect([1, 2, 1, 3, 3].uniq).to eq([1, 2, 3])
+    end
   end
 
+  describe "#two_sum" do
+    it "finds all pairs of positions who elements sum to zero" do
+      expect([-1, 0, 2, -2, 1].two_sum).to eq([[0, 4], [2, 3]])
+    end
+  end
 
+  describe "#tranpose" do
+    let(:array) {
+      rows = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8]
+      ] 
+    }
+    it "switches the rows and columns of two-dimensional array" do
+      let(:tranposed) {
+        rows = [
+          [0, 3, 6],
+          [1, 4, 7],
+          [2, 5, 8]
+        ]
+      }
+      expect(array.tranpose)to eq(tranposed)
+    end
+    it "all rows should be of equal length" do
+      
+    end
+  end
 end
