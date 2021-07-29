702cefb5b7cc7ca82591a6b1c9ce160e7d3ab50a
Mike <letumichael@gmail.com>
2021-07-28 17:57:13 -0700

    Added Word Chain Project skeleton

--- Week 3/W3D3/word_chains.rb
+++ Week 3/W3D3/word_chains.rb
@@ -7,46 +7,4 @@ class WordChainer
     @dictionary = File.readlines(dictionary_file_name).map(&:chomp)
     @dictionary = Set.new(@dictionary)
   end
-
-  def run(source, target)
-    @current_words, @all_seen_words = [source], { source => nil }
-
-    until @current_words.empty? || @all_seen_words.include?(target)
-      explore_current_words
-    end
-
-    build_path(target)
-  end
-
-  def adjacent_words(word)
-    adjacent_words = []
-
-    word.each_char.with_index do |old_letter, i|
-      ('a'..'z').each do |new_letter|
-
-        next if old_letter == new_letter
-
-        new_word = word.dup
-        new_word[i] = new_letter
-
-        adjacent_words << new_word if dictionary.include?(new_word)
-      end
-    end
-
-    adjacent_words
-  end
-
-  def explore_current_words
-    new_current_words = []
-    @current_words.each do |current_word|
-      adjacent_words(current_word).each do |adjacent_word|
-        next if @all_seen_words.key?(adjacent_word)
-
-        new_current_words << adjacent_word
-        @all_seen_words[adjacent_word] = current_word
-      end
-    end
-
-    @current_words = new_current_words
-  end
 end
