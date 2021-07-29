7af950906e6fb809ff3bf35621ca7e1c0c3397a2
Mike <letumichael@gmail.com>
2021-07-28 17:47:46 -0700

    Added Word Chain Project Skeleton and #run

--- Week 3/W3D3/word_chains.rb
+++ Week 3/W3D3/word_chains.rb
@@ -7,4 +7,14 @@ class WordChainer
     @dictionary = File.readlines(dictionary_file_name).map(&:chomp)
     @dictionary = Set.new(@dictionary)
   end
+
+  def run(source, target)
+    @current_words, @all_seen_words = [source], { source => nil }
+
+    until @current_words.empty? || @all_seen_words.include?(target)
+      explore_current_words
+    end
+
+    build_path(target)
+  end
 end
