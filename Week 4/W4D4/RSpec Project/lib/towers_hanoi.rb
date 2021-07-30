788305ba437e50293dcfefb04592be5b183ead76
Mike <letumichael@gmail.com>
2021-07-29 17:30:49 -0700

    Finished all methods except for #play

--- Week 4/W4D4/RSpec Project/lib/towers_hanoi.rb
+++ Week 4/W4D4/RSpec Project/lib/towers_hanoi.rb
@@ -1,31 +1,32 @@
 require "pp"
 
 class TowersHanoi
-  attr_reader :num_discs, :stacks
+  attr_accessor :num_discs, :stacks, :stack1
 
   def initialize(num_discs)
     @num_discs = num_discs
-    stack1 = (1..num_discs).to_a.reverse
+    @stack1 = (1..num_discs).to_a.reverse
     @stacks = [ stack1, [], [] ]
   end
 
   def play
     won?
-    move
+    move(*get_input)
   end
 
   def won?
     stacks[-1] == (1..num_discs).to_a.reverse
   end
 
-  def move
-    ""
+  def move(from, destination)
+    raise "Can't move a disc from an empty stack" if stacks[from].empty?
+    raise "Can't a move a larger disc on top of a smaller one" if !stacks[destination].empty? && (stacks[from].last > stacks[destination].last)
+    stacks[destination] << stacks[from].pop
   end
 
-  # def print
-  #   # pp self.stacks
-  # end
-end
+  def get_input
+    puts "Enter the stack you would like to move from, and its destination stack"
+    move = gets.chomp.split.map(&:to_i)
+  end
 
-# tower = TowersHanoi.new(3)
-# p tower.print
+end
--- Week 4/W4D4/RSpec Project/spec/towers_hanoi_spec.rb
+++ Week 4/W4D4/RSpec Project/spec/towers_hanoi_spec.rb
@@ -17,7 +17,34 @@ describe TowersHanoi do
       expect(tower5.stacks).to eq( [[5, 4, 3, 2, 1], [], []] )
     end
 
-    
+
+  end
+
+
+  describe "#won?" do
+
+
+    context "when all discs are in the final pile from largest to smallest" do
+      it "should return true" do
+        won_array = [3, 2, 1]
+
+        tower.stacks[-1] = won_array
+        expect(tower.won?).to eq(true)
+      end
+    end
+
+    context "when all discs are not in the final pile in order of largest to smallest" do
+      it "should return false" do
+        expect(tower.won?).to eq(false)
+      end
+    end
+  end
+
+  describe "#move" do
+    let(:tower_bad) { TowersHanoi.new(3) }
+    it "should raise an error if from stack is empty" do
+      expect { tower.move(1, 2) }.to raise_error("Can't move a disc from an empty stack")
+    end
   end
 
   describe "#play" do
@@ -32,23 +59,4 @@ describe TowersHanoi do
     end
 
   end
-
-  describe "#won?" do
-    context "when all discs are in the final pile" do
-      it "should return true" do
-        expect(tower.stacks[-1]).to eq((1..tower.num_discs).to_a.reverse)
-        expect(tower.won?).to eq(true)
-      end
-    end
-
-    context "when all discs are not in the final pile" do
-      it "should return false" do
-        expect(tower.stacks[-1]).not_to eq((1..tower.num_discs).to_a.reverse)
-        expect(tower.won?).to eq(false)
-      end
-    end
-  end
-
-
-
 end
