8ddb916841aae31538b840aa46d41ae62a2f798a
Danny Park <danincali@gmail.com>
2021-08-03 13:57:42 -0700

    SQLZOO PT1

--- Week 5/W5D2/SQL Zoo/import_db.sh
+++ Week 5/W5D2/SQL Zoo/import_db.sh
@@ -2,4 +2,5 @@
 
 dropdb sqlzoo
 createdb sqlzoo
+# fill the sqlzoo databae with < file
 psql sqlzoo < data/create_tables.sql
--- Week 5/W5D2/SQL Zoo/lib/01_select_basics.rb
+++ Week 5/W5D2/SQL Zoo/lib/01_select_basics.rb
@@ -33,29 +33,60 @@ def select_population_of_germany
 end
 
 def per_capita_gdp
-  # Show the name and per capita gdp (gdp/population) for each country where
-  # the area is over 5,000,000 km^2
+  # Show the name and per capita gdp (gdp/population) 
+  # for each country wherethe area is over 5,000,000 km^2
   execute(<<-SQL)
+    SELECT 
+      name, gdp/population AS gdp_per_capita
+    FROM
+      countries
+    WHERE
+      area > 5000000
+    
   SQL
 end
 
 def small_and_wealthy
-  # Show the name and continent of countries where the area is less than 2,000
-  # and the gdp is more than 5,000,000,000.
+  # Show the name and continent of countries where 
+  # the area is less than 2,000 and the gdp is more than 
+  # 5,000,000,000.
   execute(<<-SQL)
+
+  SELECT
+    name, continent
+  FROM
+    countries
+  WHERE
+    area<2000 AND gdp>5000000000
+
   SQL
 end
 
 def scandinavia
-  # Show the name and the population for 'Denmark', 'Finland', 'Norway', and
+  # Show the name and the population for 'Denmark', 
+    # 'Finland', 'Norway', and
   # 'Sweden'
   execute(<<-SQL)
+    SELECT
+      name, population
+    FROM 
+      countries
+    WHERE
+      name IN ('Denmark','Finland', 'Norway','Sweden')
+    
   SQL
 end
 
 def starts_with_g
   # Show each country that begins with the letter G
   execute(<<-SQL)
+    SELECT
+      name
+    FROM
+      countries
+    WHERE
+      name LIKE 'G%'
+
   SQL
 end
 
