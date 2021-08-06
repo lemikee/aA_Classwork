939c0dc644524c53284dd8f0fd82945a38ecd92f
Mauricio <mjlomeli@uci.edu>
2021-08-06 16:00:35 -0700

    finished harrison ford

--- Week 5/W5D5/Movie Buff/movie_buff/03_queries.rb
+++ Week 5/W5D5/Movie Buff/movie_buff/03_queries.rb
@@ -1,13 +1,17 @@
 def what_was_that_one_with(those_actors)
   # Find the movies starring all `those_actors` (an array of actor names).
   # Show each movie's title and id.
-
-  
+  Movie
+    .select(:title, :id)
+    .joins(:actors)
+    .where(actors: { name: those_actors })
+    .group(:id)
+    .having('COUNT(actors.id) = ?', those_actors.size)
 end
 
 def golden_age
   # Find the decade with the highest average movie score.
-
+  
 end
 
 def costars(name)
