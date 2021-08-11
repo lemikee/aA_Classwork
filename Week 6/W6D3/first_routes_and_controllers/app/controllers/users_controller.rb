0a3ed72dfab50bf3e88af1481dbf78fc20a6199c
Mike Le <letumichael@gmail.com>
2021-08-11 15:10:51 -0700

    Created Users controller

--- Week 6/W6D3/first_routes_and_controllers/app/controllers/users_controller.rb
+++ Week 6/W6D3/first_routes_and_controllers/app/controllers/users_controller.rb
@@ -22,4 +22,34 @@ class UsersController < ApplicationController
       render json: @user.errors.full_messages, status: 422
     end
   end
+
+  def update
+    @user = User.find_by(id: params[:id])
+    unless @user
+      render json: { error: 'There is no user with that id.' }
+      return
+    end
+
+    if @user.update(user_params)
+      redirect_to user_url(@user)
+    else
+      render json: @user.errors.full_messages, status: 422
+    end
+  end
+
+  def destroy
+    @user = User.find_by(id: params[:id])
+    if @user
+      @user.destroy
+      redirect_to users_url
+    else
+      render json: { error: 'There is no user with that id.' }, status: 404
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:body)
+  end
 end
