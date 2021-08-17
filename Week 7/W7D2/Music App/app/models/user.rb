def90df7546e61222ac9b4fbcaf6242f5f92386b
Mike Le <letumichael@gmail.com>
2021-08-17 14:35:36 -0700

    Added validation to user model for user auth and attributes

--- Week 7/W7D2/Music App/app/models/user.rb
+++ Week 7/W7D2/Music App/app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ApplicationRecord
+  validates :activation_token, :email, :session_token, uniqueness: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+  validates :email,
+            :password_digest,
+            :session_token,
+            :activation_token,
+            presence: true
+
+  attr_reader :password
+
+  after_initialize :ensure_session_token
+  after_initialize :set_activation_token
 end
