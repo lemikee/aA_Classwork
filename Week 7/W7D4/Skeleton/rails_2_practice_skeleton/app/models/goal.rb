d8a39964467bb10e2eddbe71686c2ab1f11c9dc5
JoeJoegb922 <josephlam922@gmail.com>
2021-08-19 14:39:52 -0700

    created user and goals model and migrations

--- Week 7/W7D4/Skeleton/rails_2_practice_skeleton/app/models/goal.rb
+++ Week 7/W7D4/Skeleton/rails_2_practice_skeleton/app/models/goal.rb
@@ -14,11 +14,5 @@
 #
 #  index_goals_on_user_id  (user_id)
 #
-require 'rails_helper'
-
-RSpec.describe Goal, type: :model do
-  it { should validate_presence_of(:name) }
-  it { should validate_presence_of(:details) }
-  it { should allow_value(true, false).for(:status) }
-  it { should belong_to(:user) }
+class Goal < ApplicationRecord
 end
--- Week 7/W7D4/Skeleton/rails_2_practice_skeleton/spec/models/user_spec.rb
+++ Week 7/W7D4/Skeleton/rails_2_practice_skeleton/spec/models/user_spec.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  username        :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_session_token  (session_token) UNIQUE
+#
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
