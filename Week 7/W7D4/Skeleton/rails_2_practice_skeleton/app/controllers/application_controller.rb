61d49050b0cc8e67687657ec620d52fd7bc7886e
Mike Le <letumichael@gmail.com>
2021-08-19 16:48:16 -0700

    Fixed bug with _main_nav partial

--- Week 7/W7D4/Skeleton/rails_2_practice_skeleton/app/controllers/application_controller.rb
+++ Week 7/W7D4/Skeleton/rails_2_practice_skeleton/app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   def current_user
     @current_user ||= User.find_by(session_token: session[:session_token])
   end
-
+  
   def require_logged_in
     redirect_to new_session_url unless logged_in?
   end
--- Week 7/W7D4/Skeleton/rails_2_practice_skeleton/app/controllers/users_controller.rb
+++ Week 7/W7D4/Skeleton/rails_2_practice_skeleton/app/controllers/users_controller.rb
@@ -3,20 +3,33 @@ class UsersController < ApplicationController
     before_action :require_logged_in, only: [:index, :show]
 
     def index
-
+        @users = Users.all
+        render :index
     end
 
     def show
-
+        @user = User.find_by(id: params[:id])
+        render :show
     end
 
     def new
-
+        @user = User.new
+        render :new
     end
 
     def create
-
+        @user = User.new(user_params)
+        if @user.save
+            login(@user)
+            redirect_to users_url
+        else
+            flash.now[:errors] = @user.errors.full_message
+            render :new
+        end
     end
 
-    
+    private
+    def user_params
+        params.require(:user).permit(:username, :password)
+    end
 end
