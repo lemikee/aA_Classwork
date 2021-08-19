792bfdfac293a88e0d5cd8d3822610130193e3b6
JoeJoegb922 <josephlam922@gmail.com>
2021-08-19 16:16:41 -0700

    routes

--- Week 7/W7D4/Skeleton/rails_2_practice_skeleton/app/controllers/users_controller.rb
+++ Week 7/W7D4/Skeleton/rails_2_practice_skeleton/app/controllers/users_controller.rb
@@ -1,2 +1,22 @@
 class UsersController < ApplicationController
+
+    before_action :require_logged_in, only: [:index, :show]
+
+    def index
+
+    end
+
+    def show
+
+    end
+
+    def new
+
+    end
+
+    def create
+
+    end
+
+    
 end
