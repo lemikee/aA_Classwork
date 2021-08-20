5a182afd4d8a8d62403c13d5ce422aa93e67bb91
Mike Le <letumichael@gmail.com>
2021-08-19 18:03:41 -0700

    Removed project folder

--- Week 7/W7D4/Skeleton/rails_2_practice_skeleton/config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  resource :session, only: [:create, :destroy, :new]
-  resources :users, only: [:index, :show, :create, :new] do
-    resources :goals, only: [:create]
-  end
-  resources :goals, only: [:destroy]
-end
