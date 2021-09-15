6fd52aa0cff304c3882e6b0e444c35b4f1884e4b
Mike Le <letumichael@gmail.com>
2021-09-14 17:06:01 -0700

    Tested creating, toggling and destroying steps

--- Week 11/W11D2/Todos/frontend/reducers/steps_reducer.js
+++ Week 11/W11D2/Todos/frontend/reducers/steps_reducer.js
@@ -1,4 +1,4 @@
-import { RECEIVE_STEPS, RECEIVE_STEPS, REMOVE_STEP } from "../actions/step_actions";
+import { RECEIVE_STEPS, RECEIVE_STEP, REMOVE_STEP } from "../actions/step_actions";
 
 const initialState = {
   1: { // this is the step with id = 1
@@ -40,3 +40,27 @@ const stepsReducer = (state = initialState, action) => {
 };
 
 export default stepsReducer;
+
+const newSteps = [
+  { id: 1, title: 'Dispatch actions', done: false, todo_id: 1 },
+  { id: 2, title: 'Dispatch more actions', done: false, todo_id: 2 }
+];
+
+
+// const newTodos = [{ id: 1, title: 'Learn Redux', body: 'It is fundamental', done: false }, { id: 2, ...etc }, ...etc];
+// const newSteps = [{ id: 1, title: 'Dispatch actions', done: false, todo_id: 1 }, { id: 2, ...etc }, ...etc];
+// store.dispatch(receiveTodos(newTodos));
+// store.dispatch(receiveSteps(newSteps));
+// store.getState(); // should return only the new todos and steps
+
+// store.dispatch(receiveTodo({ id: 3, title: "New Todo" ...etc }));
+// store.dispatch(receiveStep({ id: 3, title: "New Step", todo_id: 2 done: false ...etc }));
+// store.getState(); // should include the newly added todo and step
+
+// store.dispatch(receiveTodo({ id: 3, title: "Newer Todo" ...etc }));
+// store.dispatch(receiveStep({ id: 3, title: "Newer Step", done: true ...etc }));
+// store.getState(); // should update the previously added todo and step
+
+// store.dispatch(removeTodo({ id: 3, title: "Newer Todo" ...etc }));
+// store.dispatch(removeStep({ id: 3, title: "Newer Step" ...etc }));
+// store.getState(); // should not include the previously added todo
