79f0f1652fc5b6e23cdd77585b83452c8a1bce5b
Mike Le <letumichael@gmail.com>
2021-09-15 16:52:21 -0700

    Created TodoList component skeleton

--- Week 11/W11D2/Todos/frontend/components/todos/todo_list.jsx
+++ Week 11/W11D2/Todos/frontend/components/todos/todo_list.jsx
@@ -1,5 +1,16 @@
 import React from 'react'
 import TodoListContainer from './todo_list_container'
+import TodoListItem from './todo_list_item'
 import { connect } from 'react-redux'
 
 export default () => <h3>Todo List goes here!</h3>
+
+class TodoList extends React.Component {
+  
+}
+
+// const TodoList = () => {
+//   return (
+//     TodoListItem)
+
+// }
