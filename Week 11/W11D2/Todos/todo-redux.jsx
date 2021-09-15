86de7e6eb56de0adc347f1eca186e3ce39fa1d52
Mike Le <letumichael@gmail.com>
2021-09-15 14:24:02 -0700

    Added react structure to redux project

--- Week 11/W11D2/Todos/todo-redux.jsx
+++ Week 11/W11D2/Todos/todo-redux.jsx
@@ -1,20 +1,28 @@
 import configureStore from "./frontend/store/store";
+import React from "react";
+import ReactDOM from 'react-dom';
 import { receiveTodo, receiveTodos, removeTodo } from "./frontend/actions/todo_actions";
 import { receiveStep, receiveSteps, removeStep } from "./frontend/actions/step_actions";
-
-
+import App from "./frontend/components/app";
+import Root from './frontend/components/root';
 
 
 
 document.addEventListener("DOMContentLoaded", () => {
-
   const store = configureStore();
+  window.store = store;
+
   window.receiveTodo = receiveTodo;
   window.receiveTodos = receiveTodos;
   window.removeTodo = removeTodo;
   window.receiveSteps = receiveSteps;
   window.receiveStep = receiveStep;
   window.removeStep = removeStep;
-  window.store = store;
+
+  const main = document.getElementById('main');
+  ReactDOM.render(<Root store={store}/>, main);
+
   console.log("Welcome to your todo list!");
 });
+
+
