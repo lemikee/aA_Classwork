b2bec613cfc233a7016477145519edf9f21b8b03
Mike Le <letumichael@gmail.com>
2021-09-16 16:19:26 -0700

    Added routes and seeded db

--- Week 11/W11D2/Todos Phase 3/db/seeds.rb
+++ Week 11/W11D2/Todos Phase 3/db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+todos1 = Todo.create(
+  title: 'walk the doggie',
+  body: 'at the park',
+  done: false
+)
+
+todos2 = Todo.create(
+  title: 'go to the store',
+  body: 'buy groceries',
+  done: false
+)
